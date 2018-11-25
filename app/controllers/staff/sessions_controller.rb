6c523b97c6fbe3eb4e85d4b28396a57bd63fcc88
kaneda <kaneda@shands.jp>
2018-11-25 15:12:26 +0900

    14-2 - 職員のログイン・ログアウト記録
    14-2 - 職員のログイン・ログアウト記録

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -13,16 +13,18 @@ class Staff::SessionsController < Staff::Base
   def create
     @form = Staff::LoginForm.new(params[:staff_login_form])
     if @form.email.present?
-      staff_member= StaffMember.find_by(email_for_index: @form.email.downcase)
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
     end
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
       if staff_member.suspended?
+        staff_member.events.create!(type: 'rejected')
         flash.now.alert = 'アカウントが停止されています。'
         render action: 'new'
       else
         session[:staff_member_id] = staff_member.id
         session[:last_access_time] = Time.current
-        flash.notice = 'ログインしました'
+        staff_member.events.create!(type: 'logged_in')
+        flash.notice = 'ログインしました。'
         redirect_to :staff_root
       end
     else
@@ -32,6 +34,9 @@ class Staff::SessionsController < Staff::Base
   end
 
   def destroy
+    if current_staff_member
+      current_staff_member.events.create!(type: 'logged_out')
+    end
     session.delete(:staff_member_id)
     flash.notice = 'ログアウトしました。'
     redirect_to :staff_root
