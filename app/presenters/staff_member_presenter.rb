5d59e96db6ce8cf2b452c323f1e4f86d099bc889
kaneda <kaneda@shands.jp>
2018-12-05 03:38:53 +0900

    16-Ex - 職員の氏名全体を生成するメソッド
    16-Ex - 職員の氏名全体を生成するメソッド

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,9 +1,18 @@
 class StaffMemberPresenter < ModelPresenter
+
+  delegate :suspended?, to: :object
+
+  def full_name
+    object.family_name + ' ' + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + ' ' + object.given_name_kana
+  end
+
   # 職員の停止フラグのON/OFFを表現する記号を返す。
   #   ON: BALLOT BOX WITH CHECK (U+2611)
   #   OFF: BALLOT BOX (U+2610)
-
-  delegate :suspended?, to: :object
 
   def suspended_mark
     suspended? ? view_context.raw('&#x2611;') : view_context.raw('&#x2610;')
